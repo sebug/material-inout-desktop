48d52fd9b08140a2ce2fa744eea6e831624a5f23
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-10 15:20:52 +0200

    Referencing file picker

--- ImportArticlesPage.xaml.cs
+++ ImportArticlesPage.xaml.cs
@@ -6,5 +6,19 @@ public partial class ImportArticlesPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void ImportButtonClicked(object sender, EventArgs args)
+    {
+        var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.macOS, new List<string> { "xlsx" } }
+        });
+        var options = new PickOptions
+        {
+            PickerTitle = "Importer fichier d'articles",
+            FileTypes = customFileType
+        };
+        FilePicker.Default.PickAsync(options);
+    }
 }
 
