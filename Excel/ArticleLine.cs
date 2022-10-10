bcf163d80414e2a1b8728f8684585411527da550
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-10 16:12:57 +0200

    With article line

---
+++ Excel/ArticleLine.cs
@@ -0,0 +1,6 @@
+namespace material_inout_desktop.Excel;
+
+public record ArticleLine(string Mnemonic, string Label, string EAN)
+{
+    
+}
--- Excel/IArticlesListReader.cs
+++ Excel/IArticlesListReader.cs
@@ -2,5 +2,5 @@ namespace material_inout_desktop.Excel;
 
 public interface IArticlesListReader
 {
-    
+    List<ArticleLine> ReadExcelFile(byte[] bytes);
 }
