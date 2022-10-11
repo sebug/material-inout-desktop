ba9bad8c97a887a7420098c2873ace224638deed
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-11 17:02:11 +0200

    Adding Address

--- VoucherDetailPage.xaml.cs
+++ VoucherDetailPage.xaml.cs
@@ -23,14 +23,43 @@ public partial class VoucherDetailPage : ContentPage
     <!DOCTYPE html>
     <html>
     <head>
+    <style>
+    body {
+        font-family: sans-serif;
+    }
+    th {
+        text-align: left;
+        padding-left: 1em;
+    }
+    td {
+        padding-left: 1em;
+    }
+    .signature {
+        padding-bottom: 5em;
+        border-bottom: 1px solid black;
+    }
+    .address {
+        float: right;
+        clear: both;
+    }
+    h1 {
+        clear: both;
+    }
+    </style>
     </head>
     <body>
     <p><button class=""print"">Print</button></p>
+    <p class=""address"">ORPC Valavran,<br />
+Rue du Village 27<br />
+1294 Genthod<br />
+Tél. +41 22 774 08 06</p>
     <h1>Bon de Sortie {{voucherId}}</h1>
-    <p>Pour: {{name}}</p>
+    <p>Responsable: {{name}}</p>
     <p>Date de création: {{createdDate}}</p>
 
     {{lines}}
+
+    <p class=""signature"">Signature</p>
 
     <script>
     let printButton = document.querySelector('.print');
